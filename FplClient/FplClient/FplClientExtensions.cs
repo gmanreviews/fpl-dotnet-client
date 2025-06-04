25714baa630b59fee2d14780355885222c77ddc8
Andrew Robinson <andrewneilrobinson@gmail.com>
2025-06-03 20:53:21 -0400

    intial commits
    nothing properly done yet; just making starting projects

---
+++ FplClient/FplClient/FplClientExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FplClient;
+
+public static class FplClientExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddHttpClient();
+        return services;
+    }
+}
